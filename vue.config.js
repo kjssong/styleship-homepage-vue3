8011c92b1db4a100a6137082ba8d39403cfb4fd1
kjssong <43169339+kjssong@users.noreply.github.com>
2022-04-12 15:52:46 +0900

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+    transpileDependencies: true,
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+        @import "@/assets/sass/_variables.scss";
+        @import "@/assets/sass/_mixin.scss";
+        @import "@/assets/sass/_opt-default.scss";
+        @import "@/assets/sass/_normalize.scss";
+      `
+            }
+        }
+    },
+    publicPath: process.env.BASE_URL,             // 실제 서비스 될 루트 경로 설정
+    outputDir: 'dist/'+process.env.BASE_URL 
+});
+
+
+/*
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+    @import "@/assets/sass/_variables.scss";
+    @import "@/assets/sass/_mixin.scss";
+  `
+            }
+        }
+    }
+}*/
